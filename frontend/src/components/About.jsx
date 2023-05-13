dc6be683f914d8053736b0e0f65563c81f81279b
Khushi <khushi.y2k1609@gmail.com>
2023-05-13 17:21:36 +0530

    todo list

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,9 @@
+const About=()=>{
+    return(
+        <>
+            <h1 style={{color:"#333652",fontSize:"2.5em",fontFamily:"Clarendon"}}>Khushi Shah<br></br><br></br>202212022</h1>
+        </>
+    )
+}
+
+export default About;
